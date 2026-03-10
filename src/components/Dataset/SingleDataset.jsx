5273a873914aedb7d46d6aea080f8aeac3fb0117
Rowland <rowland.h.mosbergen@gmail.com>
2026-03-10 21:34:38 +1100

    Adding project button in dataset component

--- src/components/Dataset/SingleDataset.jsx
+++ src/components/Dataset/SingleDataset.jsx
@@ -492,6 +492,8 @@ export default function SingleDataset() {
                         {/*<Button variant="outlined" sx={{ mr: 2, mt: 1 }} onClick={() => navigate('/dashboard')}>All Samples View</Button> */}
                         <Button variant="outlined" sx={{ mr: 2, mt: 1 }} onClick={() => navigate('/datasets')}>All Datasets View</Button>
                         <Button variant="outlined" sx={{ mr: 2, mt: 1 }} onClick={() => navigate('/patients')}>All Samples Summary</Button>
+                        <Button variant="outlined" sx={{ mr: 2, mt: 1 }} onClick={() => navigate(`/project/${datasetData.project_id}`)}>Go to Project</Button>
+
 
                         {/* hard coded */}
                         <Button variant="outlined" sx={{ mt: 1 }} onClick={() => navigate(`/dataset/details/${datasetId}`)}>Files for this dataset</Button>
